1d6aab2826cdc0b4f1a9c0440078f55853927682
boxybird <andrew.rhyand@gmail.com>
2026-01-19 08:25:17 -0700

    Add Composer autoload files and classmap for plugin dependencies

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'boxybird/ucp-checkout-for-woocommerce',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '2db3eeaf825eaf4b1ebb22f6abb1c5735bce2f05',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'boxybird/ucp-checkout-for-woocommerce' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '2db3eeaf825eaf4b1ebb22f6abb1c5735bce2f05',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
